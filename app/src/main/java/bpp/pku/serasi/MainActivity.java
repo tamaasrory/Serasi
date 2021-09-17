0070900317b9b07047f0c1ac932e9af1614c1bda
tamaasrory <tamaasrory@gmail.com>
2021-09-17 16:14:33 +0700

    Initial commit

---
+++ app/src/main/java/bpp/pku/serasi/MainActivity.java
@@ -0,0 +1,179 @@
+package bpp.pku.serasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String websiteURL = "https://serasi-bpp.pekanbaru.go.id";
+    private WebView webview;
+    SwipeRefreshLayout mySwipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!CheckNetwork.isInternetAvailable(this)) //returns true if internet available
+        {
+            //if there is no internet do this
+            setContentView(R.layout.activity_main);
+            //Toast.makeText(this,"No Internet Connection, Chris",Toast.LENGTH_LONG).show();
+
+            new AlertDialog.Builder(this) //alert the person knowing they are about to close
+                    .setTitle("Tidak ada koneksi internet")
+                    .setMessage("Silahkan periksa jaringan data atau wifi anda.")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    //.setNegativeButton("No", null)
+                    .show();
+
+        } else {
+            //Webview stuff
+            webview = findViewById(R.id.webView);
+            webview.getSettings().setJavaScriptEnabled(true);
+            webview.getSettings().setDomStorageEnabled(true);
+            webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+            webview.loadUrl(websiteURL);
+            webview.setWebViewClient(new WebViewClientDemo());
+
+        }
+        //Swipe to refresh functionality
+        mySwipeRefreshLayout = (SwipeRefreshLayout) this.findViewById(R.id.swipeContainer);
+
+        mySwipeRefreshLayout.setOnRefreshListener(
+                new SwipeRefreshLayout.OnRefreshListener() {
+                    @Override
+                    public void onRefresh() {
+                        webview.reload();
+                    }
+                }
+        );
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+
+                Log.d("permission", "permission denied to WRITE_EXTERNAL_STORAGE - requesting it");
+                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permissions, 1);
+            }
+
+
+        }
+
+        //handle downloading
+
+        webview.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setMimeType(mimeType);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie", cookies);
+                request.addRequestHeader("User-Agent", userAgent);
+                request.setDescription("Downloading file....");
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimeType));
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimeType));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Downloading File", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+
+    public long exitTime = 0;
+
+    //set back button functionality
+    @Override
+    public void onBackPressed() { //if user presses the back button do this
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            exitTime = System.currentTimeMillis();
+            if (webview.isFocused() && webview.canGoBack()) { //check if in webview and the user can go back
+                webview.goBack(); //go back in webview
+            }
+        } else { //do this if the webview cannot go back any further
+
+            new AlertDialog.Builder(this) //alert the person knowing they are about to close
+                    .setTitle("Keluar")
+                    .setMessage("Apakah anda ingin keluar dari aplikasi ini?")
+                    .setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("Tidak", null)
+                    .show();
+        }
+    }
+
+    private class WebViewClientDemo extends WebViewClient {
+        @Override
+        //Keep webview in app when clicking links
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            mySwipeRefreshLayout.setRefreshing(false);
+        }
+    }
+
+
+}
+
+class CheckNetwork {
+
+    private static final String TAG = CheckNetwork.class.getSimpleName();
+
+    public static boolean isInternetAvailable(Context context) {
+        NetworkInfo info = (NetworkInfo) ((ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+
+        if (info == null) {
+            Log.d(TAG, "Tidak ada koneksi internet");
+            return false;
+        } else {
+            if (info.isConnected()) {
+                Log.d(TAG, " Koneksi internet tersedia...");
+                return true;
+            } else {
+                Log.d(TAG, " Koneksi internet stabil");
+                return true;
+            }
+
+        }
+    }
+}
