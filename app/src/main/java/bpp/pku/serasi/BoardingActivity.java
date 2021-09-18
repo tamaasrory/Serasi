a9ef1c4d214f41fc65889c58827851bacc1f7ecf
trimuerisandes <trimuerisandes@gmail.com>
2021-09-18 11:03:56 +0700

    - boarding - splash screen

---
+++ app/src/main/java/bpp/pku/serasi/BoardingActivity.java
@@ -0,0 +1,13 @@
+package bpp.pku.serasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boarding);
+    }
+}
